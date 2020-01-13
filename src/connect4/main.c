10123d7197331753163b299e0aacf0f3c47d231e
BENABDERRAHMANE Aymen-Abderrahmane <benabderrahmane@e102pc12.ecole.ensicaen.fr>
2020-01-13 10:06:45 +0100

    connect4V2

---
+++ src/connect4/main.c
@@ -0,0 +1,29 @@
+/**
+ * @file game.c 
+ * @brief The folder test our game
+ * @author Aymen Benabderrahmane
+ * @version 1.0
+ * @date 6 janvier 2020
+ */
+
+int main(){
+
+    printf("===== Bienvenue dans notre jeu magnifique Connect 4======\n");
+
+    int player = 1;
+
+    while(check_winner(m, i, j))
+    {
+        player(m, player);
+
+        if(player == 1){
+            player = 2;
+        }
+        else{
+            player = 1;    
+        }
+    }
+
+
+    return 0;
+}
